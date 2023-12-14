24bd358ee518ad9fc22e76d9f4bdd9920513194e
unknown <40187949@roctilburg.nl>
2023-12-14 18:32:04 +0100

    image slider updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,44 @@
-const imageBanner = document.querySelector('.image-banner');
-let currentIndex = 0;
+let sliderImages = Array.from(document.querySelectorAll(".slide")),
+    arrowLeft = document.querySelector("#arrow-left"),
+    arrowRight = document.querySelector("#arrow-right"),
+    current = 0;
 
-function nextImage() {
-    currentIndex = (currentIndex + 1) % imageBanner.children.length;
-    updateTransform();
+// Init slider
+function startSlide() {
+    updateSlides();
 }
 
-function updateTransform() {
-    const transformValue = `translateX(${-currentIndex * 100}%)`;
-    imageBanner.style.transform = transformValue;
+// Show prev
+function slideLeft() {
+    sliderImages[current].classList.add('move-out-right');
+    current = (current - 1 + sliderImages.length) % sliderImages.length;
+    updateSlides();
 }
 
-setInterval(nextImage, 3000); // Change image every 3 seconds
+// Show next
+function slideRight() {
+    sliderImages[current].classList.add('move-out-left');
+    current = (current + 1) % sliderImages.length;
+    updateSlides();
+}
+
+function updateSlides() {
+    sliderImages.forEach((slide, index) => {
+        slide.classList.remove('prev', 'active', 'next', 'move-out-left', 'move-out-right');
+        if (index === current) {
+            slide.classList.add('active');
+        } else if (index === (current - 1 + sliderImages.length) % sliderImages.length) {
+            slide.classList.add('prev');
+        } else if (index === (current + 1) % sliderImages.length) {
+            slide.classList.add('next');
+        }
+    });
+}
+
+// Left arrow click
+arrowLeft.addEventListener("click", slideLeft);
+
+// Right arrow click
+arrowRight.addEventListener("click", slideRight);
+
+startSlide();
